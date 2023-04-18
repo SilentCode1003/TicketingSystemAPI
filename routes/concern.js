1ec6ba2be895c9930ac67dbd8a8e9d6713779ad3
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-04-18 15:29:52 +0800

    Added Script save & load Concern Master

--- routes/concern.js
+++ routes/concern.js
@@ -17,7 +17,7 @@ function isAuthAdmin(req, res, next) {
 };
 
 /* GET home page. */
-router.get('/', isAuthAdmin,  function (req, res, next) {
+router.get('/', isAuthAdmin, function (req, res, next) {
   res.render('concern', {
     title: req.session.title,
     username: req.session.username,
@@ -28,3 +28,58 @@ router.get('/', isAuthAdmin,  function (req, res, next) {
 });
 
 module.exports = router;
+
+router.get('/load', (req, res) => {
+  try {
+    let sql = `select * from master_concern_type`;
+
+    mysql.Select(sql, 'MasterConcernType', (err, result) => {
+      if (err) {
+        return res.json({
+          msg: err
+        })
+      }
+
+      console.log(helper.GetCurrentDatetime());
+
+      res.json({
+        msg: 'success',
+        data: result
+      })
+    });
+  } catch (error) {
+    res.json({
+      msg: error
+    })
+  }
+})
+
+router.post('/save', (req, res) => {
+  try {
+    let concernname = req.body.concernname;
+    let status = dictionary.GetValue(dictionary.ACT());
+    let createdby = req.session.fullname;
+    let createdate = helper.GetCurrentDatetime();
+    let data = [];
+
+    data.push([
+      concernname,
+      status,
+      createdby,
+      createdate
+    ])
+
+    mysql.InsertTable('master_concern_type', data, (err, result) => {
+      if (err) console.error(err);
+
+      res.json({
+        msg: 'success'
+      })
+    });
+
+  } catch (error) {
+    res.json({
+      msg: error
+    })
+  }
+})
