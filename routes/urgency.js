b6431cced53a392eff3f3ba95eb60676f7d337a6
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-04-19 09:22:44 +0800

    Added Script save & load Urgency Master

--- routes/urgency.js
+++ routes/urgency.js
@@ -28,3 +28,69 @@ router.get('/', isAuthAdmin, function (req, res, next) {
 });
 
 module.exports = router;
+
+router.get('/load', (req, res) => {
+  try {
+    let sql = `select * from master_urgency_type`;
+
+    mysql.Select(sql, 'MasterUrgencyType', (err, result) => {
+      if (err) {
+        return res.json({
+          msg: err
+        })
+      }
+
+      console.log(helper.GetCurrentDatetime());
+
+      res.json({
+        msg: 'success',
+        data: result
+      })
+    });
+  } catch (error) {
+    res.json({
+      msg: error
+    })
+  }
+})
+
+router.post('/save', (req, res) => {
+  try {
+    let urgencyname = req.body.urgencyname;
+    let status = dictionary.GetValue(dictionary.ACT());
+    let createdby = req.session.fullname;
+    let createdate = helper.GetCurrentDatetime();
+    let data = [];
+    let sql_check = `select * from master_urgency_type where mut_urgencyname='${urgencyname}'`;
+
+    mysql.Select(sql_check, 'MasterUrgencyType', (err, result) => {
+      if (err) console.error('Error: ', err);
+
+      if (result.length != 0) {
+        return res.json({
+          msg: 'exist'
+        })
+      }
+      else {
+        data.push([
+          urgencyname,
+          status,
+          createdby,
+          createdate
+        ])
+
+        mysql.InsertTable('master_urgency_type', data, (err, result) => {
+          if (err) console.error(err);
+
+          res.json({
+            msg: 'success'
+          })
+        });
+      }
+    })
+  } catch (error) {
+    res.json({
+      msg: error
+    })
+  }
+})
